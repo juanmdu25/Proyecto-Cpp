eb56f471b2e279d01c2daa455bd977e4dda9cfdd
Fabiana Rojas <62910802+8-bitglitch@users.noreply.github.com>
2020-11-24 20:22:32 -0400

    Actualizacion
    Mismos cambios que en la principal

--- TDADocente.h
+++ TDADocente.h
@@ -19,7 +19,8 @@ void menuDocente(){
         cout << "\t8. Mostrar las Notas                                      " << endl;
         cout << "\t9. Volver al menu principal                        " << endl;
         cin >> opcion;
-
+        system("cls");
+        
         switch(opcion){
             case '0':  
             case '1':  
@@ -29,9 +30,10 @@ void menuDocente(){
             case '5':
             case '6':
             case '7':
-            case '8': cout << "To be inputted" << endl; cin.get(); break;
-            case '9': cout << "Salida del sistema" << endl; cin.get(); break;
-            default: cout << "El caracter introducido no es valido. Escoja una de las opciones mostradas" << endl; cin.get(); 
+            case '8': cout << "To be inputted" << endl; cin.get(); cin.ignore(); break;
+            case '9': cout << "Salida del menu" << endl; cin.get(); cin.ignore(); break;
+            default: cout << "El caracter introducido no es valido. Introduzca un numero del 0-9" << endl; cin.get(); cin.ignore(); 
         }
-    }
+        system("cls");
+    }   
 }
