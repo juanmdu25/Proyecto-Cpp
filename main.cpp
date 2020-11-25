70dc1d88293a40700f94c43d884ded26d1ba9867
Fabiana Rojas <62910802+8-bitglitch@users.noreply.github.com>
2020-11-24 20:16:47 -0400

    Actualizacion
    No limpiaba pantalla y ahora las opciones inician desde el "0".

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "TDADocente.h"
+#include "TDAEstudiantes.h"
 
 using namespace std;
 
@@ -7,18 +8,22 @@ int main(){
 
     char tipoUsuario = ' ';
 
-    while(tipoUsuario != '3'){
+    while(tipoUsuario != '2'){
         cout << "\tPor favor indique el tipo de usuario que es\n" << endl;
-        cout << "\t\t1. Docente" << endl;
-        cout << "\t\t2. Estudiante" << endl;
-        cout << "\t\t3. Salir" << endl;
+        cout << "\t\t0. Docente" << endl;
+        cout << "\t\t1. Estudiante" << endl;
+        cout << "\t\t2. Salir" << endl;
         cin >> tipoUsuario;
-    
+        system("cls");
+        
         switch(tipoUsuario){
-            case '1': menuDocente(); cin.get(); cin.ignore(); break;
-            case '2': cout << "menu Estudiante" << endl; cin.get(); cin.ignore(); break;
-            case '3': cout << "Salida del sistema" << endl; cin.get(); cin.ignore(); break;
+            case '0': menuDocente(); cin.get(); cin.ignore(); break;
+            case '1': cout << "menu Estudiante" << endl; cin.get(); cin.ignore(); break;
+            case '2': cout << "Salida del sistema" << endl; cin.get(); cin.ignore(); break;
             default: cout << "El caracter introducido no es valido. Escoja una de las tres opciones" << endl; cin.get(); cin.ignore();
-    }   }
+        }
+        system("cls");
+    }
+    
     return 0;
 }
